1a2e86fa0a6fb3a4863780ee1d224a73beceb472
Adrian Ross <adriantoddross@gmail.com>
2021-06-02 14:04:27 -0400

    Pseudocode a solution

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,20 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
 
 export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+  /*
+  Cant solve with normal function, need n to be reduced indefinitely
+  Solve with recursion and a base case; but how to keep track of count w/ recursive function?
+  Solve with a while loop (while num > 1)
+
+  Create w/ let variable to hold count
+  Create variable w/ let that copies value of n
+  If n = 1, return count
+  Open while loop
+    while n > 1...
+      if n is odd: 3n + 1
+      if n is even:  n / 2
+  Close while loop
+  */
+
+  return;
 };
