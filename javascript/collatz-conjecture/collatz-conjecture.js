e77f33bbfb2057715ceb88f9250dbe7efc470a3e
Adrian Ross <adriantoddross@gmail.com>
2021-06-10 12:03:23 -0400

    Remove comments

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,22 +3,6 @@ export const steps = (number) => {
     throw new Error("Only positive numbers are allowed");
   }
 
-  /*
-  Cant solve with normal function, need n to be reduced indefinitely
-  Solve with recursion and a base case; but how to keep track of count w/ recursive function?
-  Solve with a while loop (while num > 1)
-
-  Create w/ let variable to hold count
-  Create variable w/ let that copies value of n
-  If n = 1, return count
-  Open while loop
-    while n > 1...
-    count++;
-      if n is odd: 3n + 1
-      if n is even:  n / 2
-  Close while loop
-  */
-
   let steps = 0;
   let n = number;
 
