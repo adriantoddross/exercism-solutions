50bb4311ff347ce07e626a0d1f0bb147b9e16a61
Adrian Ross <adriantoddross@gmail.com>
2021-06-11 12:14:06 -0400

    Draft pseudocode

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+  /*
+  A gigasecond is 1 billion seconds
+
+  Pseudocode:
+  Init dateAfterGigasecond variable with function argument
+  Add miliseconds to a Date object as shown below: https://stackoverflow.com/questions/7687884/add-10-seconds-to-a-date/7687926
+    let timeObject = new Date();
+    let milliseconds= 10 * 1000; // 10 seconds = 10000 milliseconds
+    timeObject = new Date(timeObject.getTime() + milliseconds);
+  Return dateAfterGigasecond
+  */
+
+  return;
 };
