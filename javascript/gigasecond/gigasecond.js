65e9c6e7430170b15b5f2592bd9433c529bf3704
Adrian Ross <adriantoddross@gmail.com>
2021-06-14 12:20:18 -0400

    Remove psuedocode & add comments for clarity

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,18 +1,6 @@
 export const gigasecond = (date) => {
-  /*
-  A gigasecond is 1 billion seconds
-
-  Pseudocode:
-  Init dateAfterGigasecond variable with function argument
-  Add miliseconds to a Date object as shown below: https://stackoverflow.com/questions/7687884/add-10-seconds-to-a-date/7687926
-    let timeObject = new Date();
-    let milliseconds= 10 * 1000; // 10 seconds = 10000 milliseconds
-    timeObject = new Date(timeObject.getTime() + milliseconds);
-  Return dateAfterGigasecond
-  */
-
-  const gigasecond = 1e9;
-  const dateAfterGigasecond = new Date(date.getTime() + gigasecond * 1000);
+  const gigasecond = 1e9; // 1,000,000
+  const dateAfterGigasecond = new Date(date.getTime() + gigasecond * 1000); // Add a million seconds in milliseconds to a given date
   console.log({ date: date, dateAfterGigasecond });
   return dateAfterGigasecond;
 };
