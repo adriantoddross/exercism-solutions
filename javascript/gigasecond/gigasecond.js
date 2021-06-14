9dab47470ea962b3ad0b1a93256d06eab647199f
Adrian Ross <adriantoddross@gmail.com>
2021-06-14 12:21:11 -0400

    Remove console.log

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,6 +1,5 @@
 export const gigasecond = (date) => {
   const gigasecond = 1e9; // 1,000,000
   const dateAfterGigasecond = new Date(date.getTime() + gigasecond * 1000); // Add a million seconds in milliseconds to a given date
-  console.log({ date: date, dateAfterGigasecond });
   return dateAfterGigasecond;
 };
