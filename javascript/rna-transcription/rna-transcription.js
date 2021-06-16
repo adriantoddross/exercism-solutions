0eeeaf6be5d7ce0856ccc3ddfb95517a9901c70e
Adrian Ross <adriantoddross@gmail.com>
2021-06-16 11:56:35 -0400

    Pseudocode

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,8 +1,10 @@
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+  
+  /*
+  const RNAstrand =  '';
+  switch statement: when case is met, add nucleotide to RNAStrand, break by default
+  return RNAstrand
+  */
+  
+  return;
 };
