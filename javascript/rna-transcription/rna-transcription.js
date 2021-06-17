ca33dc157a0dcd2804559664742f0ee89fc91125
Adrian Ross <adriantoddross@gmail.com>
2021-06-17 14:44:45 -0400

    Pseudocode addtl approaches

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,16 +1,25 @@
 export const toRna = () => {
   /*
-  const RNAstrand =  '';
-  open for loop
-    switch statement: when case is met, add nucleotide to RNAStrand, break by default
-      cases:
-        G -> C
-        C -> G
-        T -> A
-        A -> U
-    end switch statement
-  end for loop
-  return RNAstrand
+  approach 1:
+    const RNAstrand =  '';
+    open for loop
+      switch statement: when case is met, add nucleotide to RNAStrand, break by default
+        cases:
+          G -> C
+          C -> G
+          T -> A
+          A -> U
+      end switch statement
+    end for loop
+    return RNAstrand
+
+    approach 2:
+      convert string to array
+      use forEach or .map to iterate thru string w/ switch statement
+      convert array to string & return
+
+    approach 3:
+      for of loop or a while loop (since we don't need an index)
   */
 
   return;
