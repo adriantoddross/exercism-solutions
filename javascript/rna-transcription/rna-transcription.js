75ecdd89b471312420807757843577c72824c11a
Adrian Ross <adriantoddross@gmail.com>
2021-06-24 11:58:33 -0400

    Code cleanup

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,31 +1,8 @@
 export const toRna = (DNASequence) => {
-  /*
-  approach 1:
-    const RNAstrand =  '';
-    open for loop
-      switch statement: when case is met, add nucleotide to RNAStrand, break by default
-        cases:
-          G -> C
-          C -> G
-          T -> A
-          A -> U
-      end switch statement
-    end for loop
-    return RNAstrand
-
-    approach 2:
-      convert string to array
-      use forEach or .map to iterate thru string w/ switch statement
-      convert array to string & return
-
-    approach 3:
-      for of loop or a while loop (since we don't need an index)
-  */
-
   let RNASequence = "";
 
-  for (let i = 0; i < DNASequence.length; i++) {
-    switch (DNASequence[i]) {
+  for (let index = 0; index < DNASequence.length; index++) {
+    switch (DNASequence[index]) {
       case "G":
         RNASequence += "C";
         break;
