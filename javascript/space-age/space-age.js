22a5df505aaa44d0efdb2c18930ed86701567089
Adrian Ross <adriantoddross@gmail.com>
2021-06-28 12:31:25 -0400

    Draft psuedocode with @asweetscorpio

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,20 @@
 /*
-Convert seconds to days to years
-Return age
+Declare const variable for Earth year in seconds (31557600 seconds)
+Declare const object w/ planet orbital periods
+  Object properties per planet are: Orbital period
+    Mercury: orbital period 0.2408467 Earth years
+    Venus: orbital period 0.61519726 Earth years
+    Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+    Mars: orbital period 1.8808158 Earth years
+    Jupiter: orbital period 11.862615 Earth years
+    Saturn: orbital period 29.447498 Earth years
+    Uranus: orbital period 84.016846 Earth years
+    Neptune: orbital period 164.79132 Earth years
+Declare const for age in Earth years
+If Earth, return age in Earth years... else return age in Earth years * orbital period
+Alternatively, return age * orbitalPeriod[planet]
 */
 
-export const age = (planet, age) => {
+export const age = (planet, ageInSeconds) => {
   return age;
 };
