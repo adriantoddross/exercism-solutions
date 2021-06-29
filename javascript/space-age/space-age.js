529fa4f4cfa9c481ee75d8673d3306d0c65e2236
Adrian Ross <adriantoddross@gmail.com>
2021-06-29 12:42:24 -0400

    Create constant for magic number & object for orbital periods

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -16,5 +16,17 @@ Alternatively, return age * orbitalPeriod[planet]
 */
 
 export const age = (planet, ageInSeconds) => {
+  const EARTH_YEAR_IN_SECONDS = 31_557_600;
+  const orbitalPeriod = {
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Earth: 1,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132,
+  };
+
   return age;
 };
