974a6a0be6912d36b07256bdd1348f56aabc76f4
Adrian Ross <adriantoddross@gmail.com>
2021-07-01 12:58:30 -0400

    Pseudocode approaches

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/*
+  Approaches
+    1. Regex group/range to find letters a-z, case insensitive; return results of .match & .test to verify
+    2. Run a for loop & map string chars to object (excluding punctuation), if length < 26, return false; ⚠️ will have issues with alphanumeric chars
+*/
 
 export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+  throw new Error("Remove this statement and implement this function");
 };
