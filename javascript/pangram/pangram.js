16fe4c5e9655b792fd5768b211cb1bfd40d30471
Adrian Ross <adriantoddross@gmail.com>
2021-07-07 12:46:36 -0400

    Code cleanup

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,9 +1,4 @@
-/*
-  Approaches
-    1. Regex group/range to find letters a-z, case insensitive; return .search()
-    2. Create a Set of all characters in input string (filtered by regex) in and check if length === 26: https://stackoverflow.com/questions/32557299/javascript-pangram-regex
-      2.1 Use.match() to filter non alphabetic characters, then check if length === 26
-*/
+// Filter the string input down to an array of letters A-Z using .match, then create a Set of unique characters and check if the length is 26.
 
 export const isPangram = (string) =>
   new Set(string.match(/[A-Za-z]/g)).size === 26;
