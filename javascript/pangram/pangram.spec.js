ccaefd8477a1d3a3949453b7bb3ec858e653f0fe
Adrian Ross <adriantoddross@gmail.com>
2021-07-12 11:17:25 -0400

    Pass all tests

--- javascript/pangram/pangram.spec.js
+++ javascript/pangram/pangram.spec.js
@@ -13,21 +13,21 @@ describe('Pangram()', () => {
     expect(isPangram('the quick brown fox jumps over the lazy dog')).toBe(true);
   });
 
-  xtest("missing the letter 'x'", () => {
+  test("missing the letter 'x'", () => {
     expect(
       isPangram('a quick movement of the enemy will jeopardize five gunboats')
     ).toBe(false);
   });
 
-  xtest("missing the letter 'h'", () => {
+  test("missing the letter 'h'", () => {
     expect(isPangram('five boxing wizards jump quickly at it')).toBe(false);
   });
 
-  xtest('with underscores', () => {
+  test('with underscores', () => {
     expect(isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog')).toBe(true);
   });
 
-  xtest('with numbers', () => {
+  test('with numbers', () => {
     expect(isPangram('the 1 quick brown fox jumps over the 2 lazy dogs')).toBe(
       true
     );
