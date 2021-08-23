acf24aab4170fbfdc52a464215f486d3704979de
Adrian Ross <adriantoddross@gmail.com>
2021-08-23 12:18:05 -0400

    Stuck! Add research link

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -1,12 +1,10 @@
 "use strict";
 
+// Doing research at https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1166/
+
 export const annotate = (input) => {
   if (input.length <= 1) return input;
   if (!input.includes("*") && !input.includes(" ")) return input;
 
-  // handling mine surrounded by spaces
-  // when looping through a row, stop at empty space
-  // count the number of mines from the beginning of string to empty space
-  // count the number of mines from end of string to empty space
-  // Replace the space with the sum of those numbers
+  
 };
