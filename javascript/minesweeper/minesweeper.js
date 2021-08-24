1adccb66dcedb4c05411a30f10f4fe06088e7762
Adrian Ross <adriantoddross@gmail.com>
2021-08-24 12:52:06 -0400

    Add solution to comment through... im stuck af

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -1,10 +1,9 @@
 "use strict";
 
 // Doing research at https://www.javascripttutorial.net/javascript-multidimensional-array/
+// Solution to study and comment out: https://exercism.io/tracks/javascript/exercises/minesweeper/solutions/2767c0b9384942a19c412c7f89e5a1d0
 
 export const annotate = (input) => {
   if (input.length <= 1) return input;
   if (!input.includes("*") && !input.includes(" ")) return input;
-
-  
 };
